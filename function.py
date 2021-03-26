c3f0bbd4473a6c4d71f92dd5053ea87ad011be87
ASUS <prashidbhusal3000@gmail.com>
2021-03-26 14:43:36 +0545

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+def add():#function head
+   print('function start')
+
+   a=int(input("enter the first number:"))
+   b=int(input("enter the first number:"))
+   c=a+b
+   print(c)
+print('main start')
+print('function is calling')
+add()
+print('program end')
